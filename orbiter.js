e408aff750da63fbdb4cf17548f7c1699b9600f9
Jay Mollica <jaymollica@gmail.com>
2020-10-05 17:36:44 -0700

    progress

--- orbiter.js
+++ orbiter.js
@@ -2,17 +2,25 @@
 // https://codereview.stackexchange.com/questions/211796/basic-orbiting-planets-in-p5-js
 
 class Orbiter {
-    constructor(sizeRadius, orbitRadius, orbitSpeed, color, orbitAngle=0) {
+    constructor(sizeRadius, orbitRadius, orbitSpeed, junkType, orbitAngle=0) {
+        
         this.sizeRadius = sizeRadius;
         this.orbitRadius = orbitRadius;
         this.orbitSpeed = orbitSpeed;
-        this.orbitAngle = 0; // degrees relative to x axis
+        this.type = junkType;
+        this.orbitAngle = orbitAngle; // degrees relative to x axis
+
+        this.flipped = false;
+
+        this.color = this.getColor();
+
+        
 
         // The -1.5 exponent is due to Kepler's 3rd Law
         this.orbitAngleDelta = orbitSpeed * Math.pow(orbitRadius, -1.5);
-        this.x = 0 - window.innerHeight / 1.4;
-        this.y = window.innerHeight / 2;
-        this.color = color;
+        this.x = window.innerWidth / 2;
+        this.y = window.innerHeight * 2.6;
+        
     }
 
     orbit(primary) {
@@ -26,4 +34,74 @@ class Orbiter {
         fill(this.color);
         return ellipse(this.x, this.y, this.sizeRadius, this.sizeRadius);
     }
+
+    getColor() {
+        if(this.type == "DEBRIS") {
+            return "springgreen"; //"orange";
+        }
+        else if (this.type == "EARTH") {
+            return "pink";
+        }
+        else {
+            return "magenta";
+        }
+    }
+
+    inView() {
+
+        //console.log(this.x,this.previousX )
+        let dur = 2;
+        let time = 0;
+        let vel = 0.1;
+        let note;
+        let octaveIn = 4;
+        let octaveOut = 3;
+        let notes = ['D', 'E', 'F', 'G', 'A'];
+
+        if(this.sizeRadius == 10) {
+            octaveIn = 5;
+            octaveOut = 4;
+        }
+        else if(this.sizeRadius == 20) {
+            octaveIn = 4;
+            octaveOut = 3;
+        }
+        else {
+            octaveIn = 3;
+            octaveOut = 2;
+        }
+
+        if (this.x > 0 && this.previousX < 0 ) {
+            console.log("appeared");
+            let appSynth = new p5.PolySynth();
+            appSynth.play(random(notes)+octaveIn, vel, 0, dur);
+            
+        }
+
+        if(this.x > width && this.previousX < width ) {
+            console.log("disappeared");
+            let disSynth = new p5.PolySynth();
+            disSynth.play(random(notes)+octaveOut, vel, 0, dur);   
+        }
+
+        this.previousX = this.x;
+        this.previousY = this.y;
+
+    }
+
+    clicked() {
+
+        if (dist(mouseX, mouseY, this.x, this.y) < this.sizeRadius) {
+            this.flipped = !this.flipped;
+
+            if(this.flipped) {
+                this.color = "orange";
+            } 
+            else {
+                this.color = this.getColor();
+            }
+
+        }
+    }
+
 }
--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 
 window.addEventListener('load', function() {
 
-    let dataURL = "junk.json";
-
-    fetch(dataURL)
+    fetch("junk.json")
     .then(response => response.json())
     .then(data => { 
         
         //load the json data
         junk = data;
+        dataIsReady = true;
     })
     .catch(error => {
         console.log("Nope : " + error);
@@ -20,24 +19,51 @@ window.addEventListener('load', function() {
 
 //initialize global variables
 let junk;
-let apogeeRang = [];
+let apogeeRange = [];
 let periodRange = [];
 let spaceObjects = [];
-let earth = new Orbiter(window.innerHeight * 2, 0, 0, "lightblue");
+
+// init body that junk will orbit around
+let earth = new Orbiter(window.innerWidth * 2, 0, 0, "EARTH");
+
+//GLobal boolean values to help manage timing of p5 code
+let dataIsReady = false;
+let animationIsReady = false;
 
 function setup() {
     console.log("setting up");
     createCanvas(windowWidth,windowHeight);
-    background(200,180,10);
+}
 
+function draw(){
+    background(200,180,10);
+    earth.display();
+
+    if (dataIsReady){
+        prepData();    
+    }
+
+    if (animationIsReady){
+        animateData();
+    }
+}
+
+//This will be called only once in draw()
+function prepData(){
+    console.log("Preparing the data");
+
+    //Add logic here to parse/organize the data
     let apogees = [];
     let periods = [];
     // get range of some properties before we create objects
     for (let i=0; i < junk.length; i++) {
 
-        append(apogees,junk[i].APOGEE);
-        append(periods,junk[i].PERIOD);
-        console.log(junk[i].OBJECT_TYPE);
+        if(junk[i].OBJECT_TYPE == "DEBRIS" && junk[i].APOGEE < 40000) {
+
+            append(apogees,junk[i].APOGEE);
+            append(periods,junk[i].PERIOD);
+
+        }
 
     }
 
@@ -45,14 +71,15 @@ function setup() {
     apogeeRange = getMinMax(apogees);
     periodRange = getMinMax(periods);
 
-    console.log()
+    console.log(apogees);
+
     // 2nd loop create / add objects to array
     for (let i=0; i < junk.length; i++) {
 
         // the distance from orbited body (place in range between min max of apogee)
-        let x = map(junk[i].APOGEE, apogeeRange.min, apogeeRange.max, windowHeight + 100, windowHeight * 2);
+        let x = map(junk[i].APOGEE, apogeeRange.min, apogeeRange.max, (earth.sizeRadius / 2) + 100, (earth.sizeRadius / 2) + 500);
         // set the speed of the orbit (place in range between min max of period [need to invert this])
-        let speed = map(junk[i].PERIOD, periodRange.min, periodRange.max, 2000, 5000);
+        let speed = map(junk[i].PERIOD, periodRange.min, periodRange.max, 1000, 3000);
 
         // set radius according to size property of object
         let r;
@@ -68,39 +95,44 @@ function setup() {
         }
         
         // set color based on type property of object
-        let color;
-        if(junk[i].OBJECT_TYPE == 'DEBRIS') {
-            color = "magenta";
-        }
-        else if (junk[i].OBJECT_TYPE == 'ROCKET BODY') {
-            color = "orange";
-        }
-        else if (junk[i].OBJECT_TYPE == 'PAYLOAD') {
-            color = "blue";
-        }
-        else {
-            color = "white";
-        }
+        let junkType = junk[i].OBJECT_TYPE;
 
         // create new object for eaech junk
-        let j = new Orbiter(r, x, speed, color);
+        let j = new Orbiter(r, x, speed, junkType, random(0,360));
         spaceObjects.push(j);
 
     }
 
+    //Flip first boolean value back to false
+    console.log("Flipping first boolean value back to false");
+    dataIsReady = false;
+    //Flip second boolean value to true
+    console.log("Flipping second boolean value to true");
+    animationIsReady = true;    
 }
 
-function draw() {
-    background(200,180,10);
-    earth.display();
+//This will be called repeatedly in draw()
+function animateData() {
+
     if(junk) {
         for (junk of spaceObjects) {
-            junk.orbit(earth);
-            junk.display();
+
+            if(junk.type == "DEBRIS") {
+                junk.orbit(earth);
+                junk.inView();
+                junk.display();
+            }
         }
     }
     else {
         console.log("no junk yet");
+    }
+
+}
+
+function mousePressed() {
+    for (junk of spaceObjects) {
+        junk.clicked();
     }
 }
 
